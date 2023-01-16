0f7d3f49e9d3a502446b0811b080e16eaf33effd
Wadah Esam <wadah-wleed@hotmail.com>
2023-01-16 21:54:48 +0300

    feat(example): add more options to the example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,13 @@
 import moment from 'moment';
 import * as React from 'react';
-import { StyleSheet, View, Dimensions, Text, Button, Switch } from 'react-native';
+import { StyleSheet, View, Dimensions, Text, Button, Switch, Platform } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import LineChart from 'react-native-simple-line-chart';
+import d3 from 'd3';
 
-const generateData = ({ numberOfPoints }: { numberOfPoints: number }) => {
+const generateData = ({ numberOfPoints, range }: { numberOfPoints: number; range: number }) => {
   return [...Array(numberOfPoints).fill(undefined)].map((v, index) => {
-    const value = 3000 + Math.round(Math.random() * 500);
+    const value = range + Math.round(Math.random() * 500);
     const date = moment()
       .subtract(numberOfPoints - index, 'hours')
       .toDate()
@@ -23,18 +24,20 @@ const generateData = ({ numberOfPoints }: { numberOfPoints: number }) => {
 };
 
 export default function App() {
-  const [activeDataPoint, setActiveDataPoint] = React.useState<
-    undefined | number
-  >(undefined);
   const [data, setData] = React.useState(undefined);
+  const [data2, setData2] = React.useState(undefined);
   const [numberOfPoints, setNumberOfPoints] = React.useState(30);
   const [color, setColor] = React.useState('pink');
   const [isLinearGradient, setIsLinearGradient] = React.useState(false);
   const [isEndPoint, setIsEndPoint] = React.useState(true);
+  const [isAreaChart, setIsAreaChart] = React.useState(false);
+  const [isMultipleLines, setIsMultipleLines] = React.useState(false);
+  const [lineType, setLineType] = React.useState('linear');
 
   React.useEffect(() => {
-    setData(generateData({ numberOfPoints }) as any);
-  }, [numberOfPoints]);
+    setData(generateData({ numberOfPoints, range: 3000 }) as any);
+    setData2(generateData({ numberOfPoints, range: 1500 }) as any);
+  }, [numberOfPoints, isMultipleLines, isEndPoint]);
 
   if (!data) {
     return <></>;
@@ -43,76 +46,7 @@ export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={styles.container}>
-        <Text
-          style={{
-            opacity: activeDataPoint === undefined ? 0 : 1,
-            color: color,
-            fontSize: 30,
-            marginBottom: 20,
-          }}
-        >
-          {activeDataPoint || ' '}
-        </Text>
-
-        <LineChart
-          line1={{
-            data,
-            activePointConfig: {
-              color,
-              borderColor: 'black',
-              radius: 4,
-              line: true,
-              lineColor: 'gray',
-              lineOpacity: 0.7,
-              lineWidth: 1,
-              lineDashArray: [10, 5],
-              hideAfterActivePoint: true,
-            },
-            lineWidth: 4,
-            lineColor: isLinearGradient ? ['#f5dd4b', '#81b0ff'] : color,
-            fillColor: undefined,
-            endPoint: isEndPoint ? {
-              color,
-              radius: 5,
-              animated: true
-            } : undefined,
-            activePointComponent: (point) => {
-              return (
-                <View
-                  style={{
-                    backgroundColor: color,
-                    padding: 10,
-                    borderRadius: 10,
-                  }}
-                >
-                  <Text style={{ color: 'white' }}>
-                    {point?.extraData?.formattedValue}
-                  </Text>
-                  <Text style={{ color: 'white' }}>
-                    {point?.extraData?.formattedTime}
-                  </Text>
-                </View>
-              );
-            },
-          }}
-          extraConfig={{
-            endSpacing: isEndPoint ? 20 : 0,
-            alwaysShowActivePoint: false,
-            alwaysStartFromZero: false,
-            hideActivePointOnBlur: true,
-            fadeStart: false,
-            initialActivePoint: undefined,
-          }}
-          onPointFocus={(point) => {
-            setActiveDataPoint(point?.value);
-          }}
-          onPointLoseFocus={() => {
-            setActiveDataPoint(undefined);
-          }}
-          backgroundColor={undefined}
-          height={200}
-          width={Dimensions.get('screen').width}
-        />
+        <Chart data={data} data2={data2} isAreaChart={isAreaChart} color={color} isEndPoint={isEndPoint} isLinearGradient={isLinearGradient} isMultipleLines={isMultipleLines} />
 
         <View style={{ flexDirection: 'row', marginTop: 40 }}>
           <View style={{ marginHorizontal: 5 }}>
@@ -131,7 +65,6 @@ export default function App() {
             }} />
           </View>
         </View>
-
 
         {!isLinearGradient && (<View style={{ flexDirection: 'row', marginTop: 20 }}>
           <View style={{ marginHorizontal: 5 }}>
@@ -171,21 +104,173 @@ export default function App() {
           />
         </View>
 
-        <View style={{ flexDirection: 'row', marginTop: 20, alignItems: 'center' }}>
+        <View style={{ flexDirection: 'row', marginTop: Platform.select({ ios: 20, android: 5 }), alignItems: 'center' }}>
           <Text style={{ marginRight: 10, color }}>End Point</Text>
           <Switch
             trackColor={{ false: '#767577', true: 'lightgreen' }}
             thumbColor={isEndPoint ? 'lightcyan' : '#f4f3f4'}
             ios_backgroundColor="#3e3e3e"
             onValueChange={() => {
+              if (isEndPoint == false) {
+                setIsAreaChart(false);
+              }
               setIsEndPoint(!isEndPoint);
             }}
             value={isEndPoint}
           />
         </View>
 
+        <View style={{ flexDirection: 'row', marginTop: Platform.select({ ios: 20, android: 5 }), alignItems: 'center' }}>
+          <Text style={{ marginRight: 10, color }}>Area Chart</Text>
+          <Switch
+            trackColor={{ false: '#767577', true: 'lightgreen' }}
+            thumbColor={isAreaChart ? 'lightcyan' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={() => {
+              if (isAreaChart == false) {
+                setIsEndPoint(false);
+              }
+              setIsAreaChart(!isAreaChart);
+            }}
+            value={isAreaChart}
+          />
+        </View>
+
+        <View style={{ flexDirection: 'row', marginTop: Platform.select({ ios: 20, android: 5 }), alignItems: 'center' }}>
+          <Text style={{ marginRight: 10, color }}>Multiple Lines</Text>
+          <Switch
+            trackColor={{ false: '#767577', true: 'lightgreen' }}
+            thumbColor={isMultipleLines ? 'lightcyan' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={() => {
+              setIsMultipleLines(!isMultipleLines);
+            }}
+            value={isMultipleLines}
+          />
+        </View>
+
       </View>
     </GestureHandlerRootView>
+  );
+}
+
+const Chart = ({ data, data2, color, isLinearGradient, isAreaChart, isEndPoint, isMultipleLines }: any) => {
+  const [activeDataPoint, setActiveDataPoint] = React.useState<number | undefined>(undefined);
+
+  return (
+    <>
+      <Text
+        style={{
+          opacity: activeDataPoint === undefined ? 0 : 1,
+          color: color,
+          fontSize: 30,
+          marginBottom: 20,
+        }}
+      >
+        {activeDataPoint || ' '}
+      </Text>
+      <LineChart
+        line1={{
+          data,
+          activePointConfig: {
+            color,
+            borderColor: 'black',
+            radius: 4,
+            line: true,
+            lineColor: 'gray',
+            lineOpacity: 0.7,
+            lineWidth: 1,
+            lineDashArray: [10, 5],
+            hideAfterActivePoint: true,
+          },
+          lineWidth: 4,
+          lineColor: isLinearGradient ? ['#f5dd4b', '#81b0ff'] : color,
+          fillColor: isAreaChart ? color : undefined,
+          endPoint: isEndPoint ? {
+            color,
+            radius: 5,
+            animated: true
+          } : undefined,
+          activePointComponent: (point) => {
+            return (
+              <View
+                style={{
+                  backgroundColor: color,
+                  padding: 10,
+                  borderRadius: 10,
+                }}
+              >
+                <Text style={{ color: 'white' }}>
+                  {point?.extraData?.formattedValue}
+                </Text>
+                <Text style={{ color: 'white' }}>
+                  {point?.extraData?.formattedTime}
+                </Text>
+              </View>
+            );
+          },
+        }}
+        line2={isMultipleLines ? {
+          data: data2,
+          activePointConfig: {
+            color,
+            borderColor: 'black',
+            radius: 4,
+            line: true,
+            lineColor: 'gray',
+            lineOpacity: 0.7,
+            lineWidth: 1,
+            lineDashArray: [10, 5],
+            hideAfterActivePoint: true,
+          },
+          lineWidth: 4,
+          lineColor: isLinearGradient ? ['#f5dd4b', '#81b0ff'] : color,
+          fillColor: isAreaChart ? color : undefined,
+          endPoint: isEndPoint ? {
+            color,
+            radius: 5,
+            animated: true
+          } : undefined,
+          activePointComponent: (point) => {
+            return (
+              <View
+                style={{
+                  backgroundColor: color,
+                  padding: 10,
+                  borderRadius: 10,
+                  marginTop: 10
+                }}
+              >
+                <Text style={{ color: 'white' }}>
+                  {point?.extraData?.formattedValue}
+                </Text>
+                <Text style={{ color: 'white' }}>
+                  {point?.extraData?.formattedTime}
+                </Text>
+              </View>
+            );
+          },
+        } : undefined}
+        extraConfig={{
+          endSpacing: isEndPoint ? 20 : 0,
+          alwaysShowActivePoint: false,
+          alwaysStartFromZero: false,
+          hideActivePointOnBlur: true,
+          fadeStart: false,
+          initialActivePoint: undefined,
+        }}
+        onPointFocus={(point) => {
+          setActiveDataPoint(point?.value);
+        }}
+        onPointLoseFocus={() => {
+          setActiveDataPoint(undefined);
+        }}
+        backgroundColor={undefined}
+        height={200}
+        width={Dimensions.get('screen').width}
+      />
+    </>
+
   );
 }
 
