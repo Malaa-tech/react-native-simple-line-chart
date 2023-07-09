758c3af441c9d10ab02e65289c11244853aa4883
Wadah Esam <wadah-wleed@hotmail.com>
2023-07-09 22:26:22 +0300

    fix(example): second example fixes

--- example/src/App2.tsx
+++ example/src/App2.tsx
@@ -8,80 +8,82 @@ export default function App() {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <LineChart
-          line1={{
-            data: [
-              {
-                y: 10,
-                x: new Date('2020-01-01').getTime(),
-                extraData: {
-                  formattedValue: '0',
-                  formattedTime: '2020-01-01',
+          lines={[
+            {
+              data: [
+                {
+                  y: 10,
+                  x: new Date('2020-01-01').getTime(),
+                  extraData: {
+                    formattedValue: '0',
+                    formattedTime: '2020-01-01',
+                  },
                 },
-              },
-              {
-                y: 20,
-                x: new Date('2020-01-02').getTime(),
-                extraData: {
-                  formattedValue: '20',
-                  formattedTime: '2020-01-02',
+                {
+                  y: 20,
+                  x: new Date('2020-01-02').getTime(),
+                  extraData: {
+                    formattedValue: '20',
+                    formattedTime: '2020-01-02',
+                  },
                 },
-              },
-              {
-                y: 15,
-                x: new Date('2020-01-03').getTime(),
-                extraData: {
-                  date: new Date('2020-01-03').getTime(),
-                  formattedValue: '15$',
-                  formattedTime: '2020-01-03',
+                {
+                  y: 15,
+                  x: new Date('2020-01-03').getTime(),
+                  extraData: {
+                    date: new Date('2020-01-03').getTime(),
+                    formattedValue: '15$',
+                    formattedTime: '2020-01-03',
+                  },
                 },
-              },
-              {
-                y: 35,
-                x: new Date('2020-01-04').getTime(),
-                extraData: {
-                  formattedValue: '35$',
-                  formattedTime: '2020-01-04',
+                {
+                  y: 35,
+                  x: new Date('2020-01-04').getTime(),
+                  extraData: {
+                    formattedValue: '35$',
+                    formattedTime: '2020-01-04',
+                  },
                 },
-              },
-              {
-                y: 5,
-                x: new Date('2020-01-06').getTime(),
-                extraData: {
-                  formattedValue: '35$',
-                  formattedTime: '2020-01-04',
+                {
+                  y: 5,
+                  x: new Date('2020-01-06').getTime(),
+                  extraData: {
+                    formattedValue: '35$',
+                    formattedTime: '2020-01-04',
+                  },
                 },
+              ],
+              activePointConfig: {
+                color: 'black',
+                showVerticalLine: true,
               },
-            ],
-            activePointConfig: {
-              color: 'black',
-              showVerticalLine: true,
+              lineColor: 'pink',
+              curve: 'linear',
+              endPointConfig: {
+                color: 'pink',
+                radius: 5,
+                animated: true,
+              },
+              activePointComponent: (point) => {
+                return (
+                  <View
+                    style={{
+                      backgroundColor: 'pink',
+                      padding: 10,
+                      borderRadius: 10,
+                    }}
+                  >
+                    <Text style={{ color: 'white' }}>
+                      {point?.extraData?.formattedValue}
+                    </Text>
+                    <Text style={{ color: 'white' }}>
+                      {point?.extraData?.formattedTime}
+                    </Text>
+                  </View>
+                );
+              },
             },
-            lineColor: 'pink',
-            curve: 'linear',
-            endPointConfig: {
-              color: 'pink',
-              radius: 5,
-              animated: true,
-            },
-            activePointComponent: (point) => {
-              return (
-                <View
-                  style={{
-                    backgroundColor: 'pink',
-                    padding: 10,
-                    borderRadius: 10,
-                  }}
-                >
-                  <Text style={{ color: 'white' }}>
-                    {point?.extraData?.formattedValue}
-                  </Text>
-                  <Text style={{ color: 'white' }}>
-                    {point?.extraData?.formattedTime}
-                  </Text>
-                </View>
-              );
-            },
-          }}
+          ]}
           backgroundColor={undefined}
           height={200}
           width={Dimensions.get('screen').width}
