236e257144b18be7d6e362c99378c55d24af2bca
Wadah Esam <wadah-wleed@hotmail.com>
2023-07-09 12:56:00 +0300

    fix: remove hacks in favor of real solution

--- src/ActivePointComponentWrapper.tsx
+++ src/ActivePointComponentWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { I18nManager, View } from 'react-native';
 import {
   runOnJS,
@@ -9,6 +9,7 @@ import {
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
+import { useForceReRender } from './utils';
 import { AnimatedView } from './AnimatedComponents';
 import {
   ActivePointComponent,
@@ -16,22 +17,6 @@ import {
   DataPoint,
   DataPointSharedValue,
 } from './types';
-
-const useDelayedRendering = (timeInSeconds = 1000) => {
-  const [isReady, setIsReady] = useState(false);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setIsReady(true);
-    }, timeInSeconds);
-
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, []);
-
-  return isReady;
-};
 
 const ActivePointComponentWrapper = ({
   activePointPosition,
@@ -50,10 +35,10 @@ const ActivePointComponentWrapper = ({
 }) => {
   const SPACE_BETWEEN_COMPONENT_AND_LINE = 15;
   const activeComponentWidthSV = useSharedValue<number>(100);
-  const isReady = useDelayedRendering(1000);
   const [activeDataPointLocal, setActiveDataPointLocal] = useState<
     undefined | DataPoint
   >(undefined);
+  const forceRerender = useForceReRender();
 
   const componentPositionX = useDerivedValue(() => {
     const xPosition = activePointPosition.value.x;
@@ -98,17 +83,20 @@ const ActivePointComponentWrapper = ({
     };
   });
 
-  if (activePointComponent) {
-    useAnimatedReaction(
-      () => {
-        return activePointSharedValue.value;
-      },
-      (current) => {
+  useAnimatedReaction(
+    () => {
+      return activePointSharedValue.value;
+    },
+    (current, previous) => {
+      if (current !== undefined && previous === undefined) {
+        runOnJS(forceRerender)();
+      }
+      if (activePointComponent !== undefined) {
         runOnJS(setActiveDataPointLocal)(current);
-      },
-      [activePointSharedValue]
-    );
-  }
+      }
+    },
+    [activePointSharedValue]
+  );
 
   return (
     <AnimatedView
@@ -124,13 +112,11 @@ const ActivePointComponentWrapper = ({
       >
         {activePointComponentWithSharedValue !== undefined &&
           activePointComponentWithSharedValue !== undefined &&
-          isReady &&
           activePointComponentWithSharedValue(activePointSharedValue)}
 
         {activePointComponentWithSharedValue === undefined &&
           activeDataPointLocal &&
           activePointComponent !== undefined &&
-          isReady &&
           activePointComponent(activeDataPointLocal)}
       </View>
     </AnimatedView>
